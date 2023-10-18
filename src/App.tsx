e47aa28429dd056a2eec89bcba8ac4e2751978f1
Sebastian Słoma <sebastiansloma@gmail.com>
2023-10-18 12:34:17 +0200

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React, { Fragment } from "react";
 function App(): JSX.Element {
 	return(
     <Fragment>
-      
+      <form>
+        <input type="text" />
+      </form>
       </Fragment>
   )
 }
