5fba09f4ad00eef74b588973ac21fd414649945e
Sebastian Słoma <sebastiansloma@gmail.com>
2023-10-19 16:46:21 +0200

    add new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React, { Fragment, useState } from "react";
 
+type FormElement = React.FormEvent<HTMLFormElement>
 function App(): JSX.Element {
-  const[newTask, setNewTask] = useState('')
-	return(
-    <Fragment>
-      <form>
-        <input type="text" />
-      </form>
-      </Fragment>
-  )
+	const [newTask, setNewTask] = useState("");
+
+	const handleSubmit=(e: FormElement)=>{
+    e.preventDefault()
+console.log(newTask)
+  }
+
+	return (
+		<Fragment>
+			<form onSubmit={handleSubmit}>
+				<input type='text' onChange={e => setNewTask(e.target.value)} />
+				<button>Save</button>
+			</form>
+		</Fragment>
+	);
 }
 
 export default App;
