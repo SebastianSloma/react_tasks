34d16d7a7ddb4ab2f0efd4da2c2e32b826705d19
Sebastian Słoma <sebastiansloma@gmail.com>
2023-10-22 11:16:40 +0200

    add/show task

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 interface ITask {
 	name: string;
 	done: boolean;
 }
-[{}, {}];
 function App(): JSX.Element {
-	const [newTask, setNewTask] = useState<string>("");
+	const [newTask, setNewTask] = useState<string>('');
 	const [tasks, setTasks] = useState<ITask[]>([]);
 
 	const handleSubmit = (e: FormElement) => {
 		e.preventDefault();
+		addTask(newTask);
+		setNewTask('');
 	};
 	const addTask = (name: string) => {
 		const newTasks: ITask[] = [...tasks, { name, done: false }];
@@ -21,9 +22,18 @@ function App(): JSX.Element {
 	return (
 		<Fragment>
 			<form onSubmit={handleSubmit}>
-				<input type='text' onChange={e => setNewTask(e.target.value)} />
+				<input
+					type='text'
+					onChange={e => setNewTask(e.target.value)}
+					value={newTask}
+				/>
 				<button>Save</button>
 			</form>
+			{
+				tasks.map((t:ITask, i:number)=>{
+					return<h1 key={i}>{t.name}</h1>
+				})
+			}
 		</Fragment>
 	);
 }
